5492b75255f7a0bb9fe88c8999ff5b38a8aa56ad
bekturdievashaydo <bekturdiyevashaydo9@gmail.com>
2024-07-17 15:32:17 +0500

    googletrans

---
+++ main.py
@@ -0,0 +1,30 @@
+
+import logging
+from googletrans import Translator
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '7220006041:AAH6zrmiqTWl6l9T2_DN1q-4FxaasGAEO6s'
+logging.basicConfig(level=logging.INFO)
+
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+translator=Translator()
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+
+    await message.reply("Salom!\nBu bot siz kiritkan matnni rus tiliga tarjima qilib beradi!\nKerakli matnni kiriting.")
+
+
+
+@dp.message_handler()
+async def gte_data(message: types.Message):
+    translation=translator.translate(message.text,dest="rus")
+    trans_text=translation.text
+    await message.reply(trans_text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
